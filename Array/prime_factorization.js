a4f5a3e03141f12d0d88504addb439ef2b9ae572
kumar804 <kumarsinghmanish547@gmail.com>
2021-08-30 14:43:01 +0530

    Added : Ability to solve problem using JS_DataStrtucture

---
+++ Array/prime_factorization.js
@@ -0,0 +1,32 @@
+/**
+ * Extend the Prime Factorization Program to store all the Prime Factors of a number n into an array and finally display the output.
+ * 
+ */
+
+ const prompt = require('prompt-sync')();
+
+ let count = 0;
+ let isPrime = 0;
+ let result = [];
+ let number = parseInt(prompt("Enter any number to print its prime factors: "));
+ 
+ for ( i = 2; i <= number; i++ ) {
+     let a = number % i;
+     if ( a == 0) {
+         isPrime = 1;
+         for ( j = 2; j <= i / 2; j++) {
+             let b = i % j;
+             if ( b == 0) {
+                 isPrime = 0;
+                 break;
+             }
+         }
+         if ( isPrime == 1 ) {
+             result [count] = i;
+             count++;
+         }
+     }
+ }
+ if ( isPrime == 0 || isPrime == 1 ) {
+     console.log(result);
+ }
